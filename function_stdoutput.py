b60fe8f421d43dfea6fb41fdcd65b7d42244cf31
Rosie May B. Jaronel <rmayjaronel@yahoo.com>
2016-03-04 12:09:48 +0800

    changed function name from MyFunc to Funcout

---
+++ function_stdoutput.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+
+def FuncOut(name, age):
+    print "Hi! My name is" , name + " and my age is" , age
+    print "Hi! My name is %s and my age is%d" % (name, age)
+    print "Hi! My name is {} and my age is {}" .format(name, age)
+FuncOut("Mary" , 19)
